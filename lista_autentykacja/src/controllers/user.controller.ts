ecc53acbcc182102cc6ccd75745100f425d58fd4
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2022-02-02 12:30:23 +0100

    aktualizacja auth

--- lista_autentykacja/src/controllers/user.controller.ts
+++ lista_autentykacja/src/controllers/user.controller.ts
@@ -1,12 +1,44 @@
 // Uncomment these imports to begin using these cool features!
 
 import {TokenService} from '@loopback/authentication';
-import {
-  MyUserService, TokenServiceBindings, UserRepository, UserServiceBindings
-} from '@loopback/authentication-jwt';
+import {MyUserService, TokenServiceBindings, User, UserRepository, UserServiceBindings} from '@loopback/authentication-jwt';
 import {inject} from '@loopback/core';
-import {repository} from '@loopback/repository';
+import {model, property, repository} from '@loopback/repository';
+import {SchemaObject} from '@loopback/rest';
 import {SecurityBindings, UserProfile} from '@loopback/security';
+
+
+@model()
+export class NewUserRequest extends User {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+}
+
+const CredentialsSchema: SchemaObject = {
+  type: 'object',
+  required: ['email', 'password'],
+  properties: {
+    email: {
+      type: 'string',
+      format: 'email',
+    },
+    password: {
+      type: 'string',
+      minLength: 8,
+    },
+  },
+};
+
+export const CredentialsRequestBody = {
+  description: 'Wejście funkcji logującej',
+  required: true,
+  content: {
+    'application/json': {schema: CredentialsSchema}
+  }
+}
 
 export class UserController {
   constructor(
@@ -18,4 +50,6 @@ export class UserController {
     public user: UserProfile,
     @repository(UserRepository) protected userRepository: UserRepository,
   ) { }
+
 }
+
